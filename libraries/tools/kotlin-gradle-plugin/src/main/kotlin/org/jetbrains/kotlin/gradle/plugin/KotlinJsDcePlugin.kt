d976877750ee3c50c3de41816edb848db859db5e
Sergey Igushkin <hotkeytlt@gmail.com>
2017-11-16 22:25:50 +0300

    Add cacheable task classes and the logic that chooses them at runtime

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinJsDcePlugin.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinJsDcePlugin.kt
@@ -25,6 +25,7 @@ import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.tasks.SourceSet
 import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
 import org.jetbrains.kotlin.gradle.tasks.KotlinJsDce
+import org.jetbrains.kotlin.gradle.tasks.cacheableIfSupported
 import java.io.File
 
 class KotlinJsDcePlugin : Plugin<Project> {
@@ -39,7 +40,7 @@ class KotlinJsDcePlugin : Plugin<Project> {
         val kotlinTaskName = sourceSet.getCompileTaskName("kotlin2Js")
         val kotlinTask = project.tasks.findByName(kotlinTaskName) as? Kotlin2JsCompile ?: return
         val dceTaskName = sourceSet.getTaskName(DCE_TASK_PREFIX, TASK_SUFFIX)
-        val dceTask = project.tasks.create(dceTaskName, KotlinJsDce::class.java).also {
+        val dceTask = project.tasks.create(dceTaskName, cacheableIfSupported(KotlinJsDce::class.java, project)).also {
             it.dependsOn(kotlinTask)
             project.tasks.findByName("build")!!.dependsOn(it)
         }
